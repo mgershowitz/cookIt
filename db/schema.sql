a12b591c07e6b9e81710931ccce313ef0c0f011d
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-08-25 16:14:27 -0400

    schema update to include user references

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,21 @@
+DROP TABLE IF EXISTS favorite_recipes;
 DROP TABLE if EXISTS users CASCADE;
 
-
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  user_id serial unique primary key,
-  name VARCHAR(50) unique,
+  user_id SERIAL unique PRIMARY KEY,
+  username VARCHAR(50) unique,
   email VARCHAR(255) unique not null,
   password_digest TEXT not null,
-  user_created timestamp not null default now()
+  user_created TIMESTAMP not null DEFAULT now()
 );
 CREATE INDEX on users (username) ;
 CREATE INDEX on users (email) ;
 
 
 
-DROP TABLE IF EXISTS favorite_recipes;
 CREATE TABLE favorite_recipes (
 favorites_id SERIAL PRIMARY KEY NOT NULL,
-user_reference INTEGER REFERENCES users(user_id),
+user_reference INTEGER REFERENCES users(user_id) NOT NULL,
 recipe_id INT NOT NULL,
 recipe_name TEXT,
 recipe_url VARCHAR NOT NULL,
@@ -28,7 +26,7 @@ img_link TEXT NOT NULL
 DROP TABLE IF EXISTS pantry_items;
 CREATE TABLE pantry_items (
 items_id SERIAL PRIMARY KEY NOT NULL,
-user_reference INTEGER REFERENCES users(user_id),
+user_reference INTEGER REFERENCES users(user_id) NOT NULL,
 ingredient_name TEXT NOT NULL,
 created_at TIMESTAMP NOT NULL DEFAULT now()
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,18 @@
-insert into users (name, email, password_digest)
- VALUES
-  ('test', 'test', 'test'),
-  ( 'Chandler', 'cbasso@gmail.com', 'ptest1'),
-  ( 'Arlen', 'acho@gmail.com', 'ptest2'),
-  ( 'Matt', 'mattg@gmail.com', 'ptest3'),
-  ( 'Michael', 'mwerbo2@gmail.com', 'ptest4');
+-- INSERT INTO users (username, email, password_digest)
+--  VALUES
+--   ( 'Matt', 'mattg@gmail.com', 'ptest3')
 
 
 
-insert into favorite_recipes (recipe_id, recipe_name, recipe_url, img_link) VALUES
+INSERT INTO favorite_recipes (user_reference, recipe_id, recipe_name, recipe_url, img_link) VALUES
 
-  ('562994', 'Char grilled beef', 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342', 'https://spoonacular.com/recipeImages/char-grilled-beef-tenderloin-with-three-herb-chimichurri-156992.jpg');
+  (1, '562994', 'Char grilled beef', 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342', 'https://spoonacular.com/recipeImages/char-grilled-beef-tenderloin-with-three-herb-chimichurri-156992.jpg');
 
 
-insert into pantry_items ( ingredient_name) VALUES
+INSERT INTO pantry_items ( user_reference, ingredient_name) VALUES
 
-  ('Tomatoe'),
-  ('Apple'),
-  ('Potatoe'),
-  ('Beef'),
-  ('Big Sausage');
+  (1, 'Tomatoe'),
+  (1, 'Apple'),
+  (1, 'Potatoe'),
+  (1, 'Beef'),
+  (1, 'Big Sausage');
